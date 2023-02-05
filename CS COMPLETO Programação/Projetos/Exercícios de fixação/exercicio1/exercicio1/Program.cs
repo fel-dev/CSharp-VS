10792199bb1b197a315b295de269093342fa0ca1
Felipe Carneiro <felipecarneiro@duck.com>
2023-02-05 13:56:55 -0300

    finish this one

--- CS COMPLETO Programação/Projetos/Exercícios de fixação/exercicio1/exercicio1/Program.cs
+++ CS COMPLETO Programação/Projetos/Exercícios de fixação/exercicio1/exercicio1/Program.cs
@@ -1,5 +1,7 @@
 ﻿// This is a exercise Number 1, from the udemy C#'s course
 
+using System.Globalization;
+
 string product1 = "PC";
 string product2 = "Office desk";
 
@@ -13,6 +15,8 @@ double measure = 53.234567;
 
 Console.WriteLine("Products:");
 Console.WriteLine("{0}, witch price is $ {1}", product1, price1);
-Console.WriteLine($"{product2}, witch price is $ {price2}");
+Console.WriteLine($"{product2}, witch price is $ {price2.ToString("F2")}");
 Console.WriteLine("\nRegister: {0} years old, code {1} and gender: {2}", age, cod, gender);
-
+Console.WriteLine("\nMeasure with eight decimal places: {0}", measure.ToString("F8"));
+Console.WriteLine("Rouded (three decimal places): {0}", measure.ToString("F3"));
+Console.WriteLine("Separate decimal point: {0}", measure.ToString("F3",CultureInfo.InvariantCulture));
